0c6e542d131227611b7ba603799527f3093c1728
Fatima-Bee <mariadfalejo@gmail.com>
2025-08-06 07:00:07 -0400

    Activity 10

--- boilerplate/js/mainTEST.js
+++ boilerplate/js/mainTEST.js
@@ -6,7 +6,6 @@
 var attrArray = ["varA", "varB", "varC", "varD", "varE"]; //list of attributes
 var expressed = attrArray[0]; //initial attribute
 
-
 window.onload = setMap();
 
 //settingup the up choropleth map Example 1.3
@@ -39,7 +38,6 @@ var projection = d3.geoConicEqualArea()
     .scale(700)
     .translate([width / 2, height / 2]);
 
-
 //Example 2.2
     var path = d3.geoPath().projection(projection);
 
@@ -49,43 +47,17 @@ var projection = d3.geoConicEqualArea()
     promises.push(d3.json("data/NatParkpoly.topojson"));
     promises.push(d3.json("data/worldmap.topojson"));                                      
     Promise.all(promises).then(callback);
- 
-//callback funtion Example 1.4 2.3 2.5 2.6
 
+//callback funtion Example 1.4 2.3 2.5 2.6
     function callback(data) {
         var csvData = data[0],
             natparks = data[1];
             worldmap = data[2];
 
-
 //graticule generator - create graticule lines 2.6
+        setGraticule(map, path);
 
-        var graticule = d3.geoGraticule()
-//places graticule lines every 5 degrees of longitude and latitude
-            .step([5, 5]);
-
-        var gratBackground = map.append("path")
-//bind graticule background
-            .datum(graticule.outline()) 
-//assign class for styling
-            .attr("class", "gratBackground") 
-//project graticule
-            .attr("d", path) 
-
-//select graticule elements that will be created
-        var gratLines = map.selectAll(".gratLines") 
-//Attach graticule lines to each element to be created
-            .data(graticule.lines()) 
-//create an element for each datum
-            .enter() 
-//append each element to the svg as a path element
-            .append("path") 
-//assign class for styling
-            .attr("class", "gratLines") 
-//project graticule lines
-            .attr("d", path); 
-
-        //console.log(csvData);
+//console.log(csvData);
         //console.log(natparks);
 
 //console.log("Available TopoJSON objects:", Object.keys(natparks.objects));
@@ -106,19 +78,100 @@ var projection = d3.geoConicEqualArea()
             .attr("class", "country")
             .attr("d", path);
 
-        var regions = map.selectAll(".regions")
-            .data(natParkpoly.features)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "regions " + d.properties.adm1_code;
-            })
-            .attr("d", path);
+        //join csv data to GeoJSON enumeration units
+        natParkpoly.features = joinData(natParkpoly.features, csvData);
+
+        //create the color scale
+        var colorScale = makeColorScale(csvData);
+
+        //add enumeration units to the map
+        setEnumerationUnits(natParkpoly.features, map, path, colorScale);
 
         //examine the results
         //console.log(natParkpoly);
         //console.log("SVG path count:", document.querySelectorAll("path").length);
         //console.log("First path 'd' attribute:", document.querySelector("path")?.getAttribute("d"));
     }
+}; //end of setMap()
+
+function setGraticule(map, path){
+//graticule generator - create graticule lines 2.6
+
+    var graticule = d3.geoGraticule()
+//places graticule lines every 5 degrees of longitude and latitude
+        .step([5, 5]);
+
+    var gratBackground = map.append("path")
+//bind graticule background
+        .datum(graticule.outline()) 
+//assign class for styling
+        .attr("class", "gratBackground") 
+//project graticule
+        .attr("d", path) 
+
+//select graticule elements that will be created
+    var gratLines = map.selectAll(".gratLines") 
+//Attach graticule lines to each element to be created
+        .data(graticule.lines()) 
+//create an element for each datum
+        .enter() 
+//append each element to the svg as a path element
+        .append("path") 
+//assign class for styling
+        .attr("class", "gratLines") 
+//project graticule lines
+        .attr("d", path); 
+};
+
+function joinData(natParkFeatures, csvData){
+//placeholder for join logic if needed later
+    return natParkFeatures;
+};
+
+function setEnumerationUnits(natParkFeatures, map, path, colorScale){
+    var regions = map.selectAll(".regions")
+        .data(natParkFeatures)
+        .enter()
+        .append("path")
+        .attr("class", function(d){
+            return "regions " + d.properties.adm1_code;
+        })
+        .attr("d", path)
+        .style("fill", function(d){
+            var value = d.properties[expressed];
+            if (!isNaN(value)) {
+                return colorScale(value);
+            } else {
+                return "#ccc";
+            }
+        });
+};
+
+//function to create color scale generator
+function makeColorScale(data){
+    var colorClasses = [
+        "#D4B9DA",
+        "#C994C7",
+        "#DF65B0",
+        "#DD1C77",
+        "#980043"
+    ];
+
+    //create color scale generator
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
+
+    //build array of all values of the expressed attribute
+    var domainArray = [];
+    for (var i=0; i<data.length; i++){
+        var val = parseFloat(data[i][expressed]);
+        domainArray.push(val);
     };
-    })();
+
+    //assign array of expressed values as scale domain
+    colorScale.domain(domainArray);
+
+    return colorScale;
+};
+
+})();
